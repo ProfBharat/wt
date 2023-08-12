8c02d5d422fd362114ac4b64839f823fb099cda4
Bharat Kumar <bharatagape@gmail.com>
2023-08-12 17:35:44 +0530

    added notes

---
+++ rough-js/ex.js
@@ -0,0 +1,19 @@
+<html>
+    <head>
+        <title>Sample js program</title>
+    </head>
+    <body>
+        <h1 id="demo">The First JavaScript Program</h1>
+
+        <button type="button" onClick="myFunction()"> Do it</button>
+
+
+        <scrip>
+            myFunction(){
+                document.getElementById("demo").innerHTML = "Paragraph Changed"
+            }
+
+
+        </scrip>
+    </body>
+</html>
